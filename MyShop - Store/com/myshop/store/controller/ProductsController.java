83bb79f9ac688b06db9401c562ec1882780ace4f
Héctor Álvarez <hec_insti@hotmail.com>
2016-11-02 20:16:48 +0100

    Eliminada lógica para descuentos
    Eliminado método para sacar productos con precio ya de descuento, el
    modelo actualizado ahorrará esa consulta a BD

--- MyShop - Store/com/myshop/store/controller/ProductsController.java
+++ MyShop - Store/com/myshop/store/controller/ProductsController.java
@@ -19,22 +19,5 @@ public class ProductsController {
 			            .addParameter("subcat", subcategory)
 			            .addColumnMapping("product_id","ID").throwOnMappingFailure(false).executeAndFetch(Product.class);
 		 }
-		
-	}
-	
-	public List<Product> getAllWithDiscount(int category, int subcategory){
-		Sql2o sql2o = new Sql2o("jdbc:mysql://myshop.cvgrlnux4cbv.eu-west-1.rds.amazonaws.com:3306/myshop", "myshop-app", "'m:9AU7n");
-		String complexSql = "SELECT * FROM myshop.product WHERE category_id = :cat AND subcategory_id = :subcat";
-		
-		List<Product> p;
-		 try (Connection con = sql2o.open()) {
-			 p= con.createQuery(complexSql).addParameter("cat", category)
-			            .addParameter("subcat", subcategory)
-			            .addColumnMapping("product_id","ID").throwOnMappingFailure(false).executeAndFetch(Product.class);
-		}
-		for(Product producto : p){
-			producto.setPrice(producto.getPrice()*0.9);
-		}
-		return p;
 	}
 }
