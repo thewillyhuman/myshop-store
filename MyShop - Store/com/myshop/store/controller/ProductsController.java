ddd0ad38d679a2afda1312372b56cb4eb5b32487
Paloma Sierra <sierrabonet@gmail.com>
2016-11-03 02:56:28 +0100

    Solucionado para que no muestre los productos sin stock
    Signed-off-by: Paloma Sierra <sierrabonet@gmail.com>

--- MyShop - Store/com/myshop/store/controller/ProductsController.java
+++ MyShop - Store/com/myshop/store/controller/ProductsController.java
@@ -14,6 +14,7 @@ public class ProductsController {
 	 private Sql2o sql2o = new Sql2o("jdbc:mysql://myshop.cvgrlnux4cbv.eu-west-1.rds.amazonaws.com:3306/myshop", "myshop-app", "'m:9AU7n");
 
 	public List<Product> getAll(int category, int subcategory){
+		Sql2o sql2o = new Sql2o("jdbc:mysql://myshop.cvgrlnux4cbv.eu-west-1.rds.amazonaws.com:3306/myshop", "myshop-app", "'m:9AU7n");
 		String complexSql = "SELECT * FROM myshop.product WHERE category_id = :cat AND subcategory_id = :subcat AND stock>0";
 		
 		 try (Connection con = sql2o.open()) {
